890aa21382b8340c97e6e939b482ef3a6ffd09b0
Sonu Saha <ahasunos@gmail.com>
2024-09-16 20:27:48 +0530

    feat: expose methods to configure & use api wrapper
    Signed-off-by: Sonu Saha <ahasunos@gmail.com>

--- lib/api_wrapper.rb
+++ lib/api_wrapper.rb
@@ -1,8 +1,86 @@
 # frozen_string_literal: true
 
 require_relative 'api_wrapper/version'
+require_relative 'api_wrapper/api_manager'
 
+# ApiWrapper provides a unified interface for managing API interactions and caching.
+#
+# It simplifies API management and caching with sensible defaults and customization options.
+#
+# Key Features:
+# - Manages a single instance of ApiManager.
+# - Provides default and customizable configuration paths.
+# - Fetches data from API endpoints with cache support.
+# - Configures globally via a block or directly.
+#
+# Usage:
+#   # Fetch data using default configuration
+#   response = ApiWrapper.fetch_data('endpoint_key')
+#   puts response.body
+#
+#   # Configure with custom settings
+#   ApiWrapper.configure do |config|
+#     config.api_configuration_path = 'custom/path/to/config.yml'
+#     config.cache_store = CustomCacheStore.new
+#   end
+#   response = ApiWrapper.fetch_data('endpoint_key')
+#   puts response.body
 module ApiWrapper
-  class Error < StandardError; end
-  # Your code goes here...
+  # Configuration class for managing settings
+  class Configuration
+    attr_accessor :api_configuration_path, :cache_store
+
+    def initialize
+      @api_configuration_path = default_api_configuration_path
+      @cache_store = default_cache_store
+    end
+
+    private
+
+    # Default API configuration path
+    def default_api_configuration_path
+      ENV['API_CONFIGURATION_PATH'] || File.join(Dir.pwd, 'config', 'api_endpoints.yml')
+    end
+
+    # Default cache store
+    def default_cache_store
+      cache_type = ENV['CACHE_STORE_TYPE'] || 'in_memory'
+      case cache_type
+      when 'redis'
+        ApiWrapper::Cache::RedisCacheStore.new
+      else
+        ApiWrapper::Cache::CacheStore.new
+      end
+    end
+  end
+
+  class << self
+    # Accesses the configuration instance, initializing if needed
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    # Configures ApiWrapper with a block
+    def configure
+      raise ArgumentError, 'Configuration block required' unless block_given?
+
+      yield(configuration)
+      reset_api_manager!
+    end
+
+    # Returns the singleton ApiManager instance
+    def api_manager
+      @api_manager ||= ApiManager.new(configuration.api_configuration_path, cache_store: configuration.cache_store)
+    end
+
+    # Fetches data from the specified endpoint
+    def fetch_data(endpoint_key, force_refresh: false)
+      api_manager.fetch_data(endpoint_key, force_refresh: force_refresh)
+    end
+
+    # Resets the ApiManager instance
+    def reset_api_manager!
+      @api_manager = nil
+    end
+  end
 end
